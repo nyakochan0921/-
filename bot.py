cfb88f49530604a517cc2bbebd547a270194b3b7
nyakochan0921 <82825900+nyakochan0921@users.noreply.github.com>
2022-08-11 20:58:59 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,18 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
       return
-async def on_message(message):
-     if message.content == '早安':
+  if message.content == '早安':    
         await message.channel.send('今日も一日よしリーチ')
 
-client.run('MTAwNjkxNzUwNDMzNTEwMjA1Ng.G3njHh.DG2OUzEEMidMV1lj58f1kdTVx3cX7qVSVFaDKE')
+@client.event
+async def on_message(message):
+     if message.author == client.user:
+        return
+     if message.content.startswith('秋山駿'):
+        tmp = message.content.split(" ",2)
+        if len(tmp) == 1:
+             await message.channel.send("秋山駿，你算計我！")
+        else:
+             await message.channel.send(tmp[1]) 
+
+client.run('MTAwNjkxNzUwNDMzNTEwMjA1Ng.GOj-QU.DFblduucdS5rWGlRFG_9XPowEtNnQYGEe321Qc')
